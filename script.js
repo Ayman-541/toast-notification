7ef4100a1112f316246ddb955cd0177fcc56fa44
Ayman-541 <aymanwasif541@gmail.com>
2024-06-15 20:22:33 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let toastBox = document.getElementById('toastBox');
+let succassMsg = '<i class="fa-solid fa-circle-check"></i>Successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error!';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i>Invalid input, cheak again';
+function showToast(msg){
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+    setTimeout(()=>{
+        toast.remove();
+    },6000)
+}
